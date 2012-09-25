4411d2e60cf34f1e892967ba011d6958cff33be5
ivershuo <shuo@aerfa.net>
2012-09-26 01:28:48 +0800

    update

--- sublimeweibo.py
+++ sublimeweibo.py
@@ -3,8 +3,18 @@ from aweibo import weibo
 
 wb = weibo()
 
-import json, webbrowser, time, os, urllib, random
-from weibosdk import APIClient
+def show_in_file(text, syntax = 'Packages/JavaScript/JSON.tmLanguage'):
+	print text
+	tmpview = sublime.active_window().new_file()
+	try:
+		if syntax:
+			tmpview.set_syntax_file()
+		edit = tmpview.begin_edit()
+		tmpview.insert(edit, 0, text)
+		tmpview.end_edit()
+	except:
+		pass
+
 
 class WeiboaccessCommand(sublime_plugin.WindowCommand):
 	def run(self):
@@ -12,20 +22,22 @@ class WeiboaccessCommand(sublime_plugin.WindowCommand):
 
 class WeibosendCommand(sublime_plugin.WindowCommand):
 	def run(self):
-		self.window.show_input_panel("Input your status here:", "", self.on_done, None, None)
+		self.window.show_input_panel("Input your status here:", "", self.on_done, self.on_change, None)
 
 	def on_done(self, text):
-		wb.send(text)
+		if 0 < len(text) <= 140:
+			wb.send(text)
+		elif len(text) > 140:
+			self.window.show_input_panel("Input your status here:", text, self.on_done, self.on_change, None)
+			sublime.error_message("140+")			
+
+	def on_change(self, text):
+		sublime.status_message(str(140 - len(text)))
 
 class WeibogettimelineCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		statuses = wb.get_timlines(True)
-		tmpview = self.window.new_file()
-		try:
-			tmpview.set_syntax_file('Packages/JavaScript/JSON.tmLanguage')
-			edit = tmpview.begin_edit()
-			tmpview.insert(edit, 0, statuses)
-			tmpview.end_edit()
-		except:
-			pass
+		if statuses != '{}':
+			show_in_file(statuses)
+		
 			
